3c652d9efe7aa3bf1eff07ad9e93950ec3aba573
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2025-05-08 14:11:28 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        display: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        display: ['Open Sans', ...defaultTheme.fontFamily.sans],
       },
       opacity: {
         2.5: '0.025',
